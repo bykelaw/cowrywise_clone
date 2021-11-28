1a3c2aaae50e93efb8f9fc6dc64bac0a7fbf3b9e
Lawrence Ughonu <lawrenceughonu@gmail.com>
2021-11-28 01:01:19 +0100

    Resolved conflict

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -6,92 +6,94 @@ import { useValidator } from "react-joi"
 
 
 const Signin = () => {
-    
-    const { state, setData, setExplicitField, validate } = useValidator({
 
-        initialData: {
-            email: null,
-            password: null,
-        },
+  const { state, setData, setExplicitField, validate } = useValidator({
 
-        schema: Joi.object({
-            email: Joi.string()
-                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-                .required()
-                .messages({
-                    'string.base': `Email is a required field`,
-                    'string.empty': `Email cannot be an empty field`,
-                    'string.email': `Email must be valid`,
-                    'any.required': `Email is a required field`
-                }),
-            
-            password: Joi.string()
-                .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
-                .min(6)
-                .required()
-                .messages({
-                    'string.base': `Password is a required field`,
-                    'string.pattern.base': `Password must contain Uppercase, Lowercase, a number and a special character`,
-                    'string.empty': `Password cannot be an empty field`,
-                    'string.min': `Password should have a minimum length of {#limit}`,
-                    'any.required': `Password is a required field`
-                  }),
-            
+    initialData: {
+      name: null,
+      email: null,
+      password: null,
+    },
+
+    schema: Joi.object({
+      email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+        .required()
+        .messages({
+          'string.base': `Email is a required field`,
+          'string.empty': `Email cannot be an empty field`,
+          'string.email': `Email must be valid`,
+          'any.required': `Email is a required field`
         }),
-        explicitCheck: {
-            email: false,
-            password: false,
-        },
-    })
 
-    const updateEmail = (e) => {
-        e.persist()
+     
 
-        setData((old) => ({
-            ...old,
-            email: e.target.value,
-        }))
-    }
+      password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+        .min(6)
+        .required()
+        .messages({
+          'string.base': `Password is a required field`,
+          'string.pattern.base': `Password must contain Uppercase, Lowercase, a number and a special character`,
+          'string.empty': `Password cannot be an empty field`,
+          'string.min': `Password should have a minimum length of {#limit}`,
+          'any.required': `Password is a required field`
+        }),
 
-    const updatePassword = (e) => {
-        e.persist()
+    }),
+    explicitCheck: {
+      email: false,
+      password: false,
+    },
+  })
 
-        setData((old) => ({
-            ...old,
-            password: e.target.value,
-        }))
-    }
+  const updateEmail = (e) => {
+    e.persist()
 
-    const handleSubmit = (event) => {
-       validate()
-       event.preventDefault()
-    }
-    return ( 
+    setData((old) => ({
+      ...old,
+      email: e.target.value,
+    }))
+  }
 
-        <>
+
+  const updatePassword = (e) => {
+    e.persist()
+
+    setData((old) => ({
+      ...old,
+      password: e.target.value,
+    }))
+  }
+
+  const handleSubmit = (event) => {
+    validate()
+    event.preventDefault()
+  }
+
+  return (
+    <>
+
+
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Login to your account</h2>
+
+            <form className="mt-8 space-y-6" onSubmit={ handleSubmit }>
+
             
-          </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
-              <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
+
+              <div className="mt-4">
+                <label htmlFor="email" className="sr-only">Email</label>
+                <input type="text" placeholder="Email"
+                  className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600 rounded-b-md"
+                  onChange={ updateEmail }
+                  onBlur={ () => setExplicitField("email", true) }
                 />
+                <span style={ { color: "red" } }>{ state.$errors.email.map((data) => data.$message).join(",") }</span>
               </div>
+
               <div>
                 <label htmlFor="password" className="sr-only">
                   Password
@@ -104,77 +106,52 @@ const Signin = () => {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
+                  onChange={ updatePassword }
+                  onBlur={ () => setExplicitField("password", true) }
                 />
+                <span style={ { color: "red" } }>{ state.$errors.password.map((data) => data.$message).join(",") }</span>
               </div>
 
-        <div>
-            <div className="flex items-center justify-center min-h-screen bg-gray-100">
-                <div className="w-1/3 px-8 py-6 text-left bg-white shadow-lg">
-                    <h3 className="text-2xl font-bold text-center">Log in your account</h3>
-                    <form onSubmit={handleSubmit}>
-                        <div className="mt-4">
-                            <label for="email">Email</label>
-                            <input type="text" placeholder="Email"
-                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
-                            onChange={updateEmail}
-                            onBlur={() => setExplicitField("email", true)}
-                            />
-                            <span style={{color:"red"}}>{state.$errors.email.map((data) => data.$message).join(",")}</span>
-                        </div>
-                        <div className="mt-4">
-                            <label for="password">Password</label>
-                            <input type="text" placeholder="Password"
-                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
-                            onChange={updatePassword}
-                            onBlur={() => setExplicitField("password", true)}
-                            />
-                             <span style={{color:"red"}}>{state.$errors.password.map((data) => data.$message).join(",")}</span>
-                        </div>
-                        <div className="flex items-baseline justify-between">
-                            <button className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Login</button>
-                            <a href="#" className="text-sm text-blue-600 hover:underline">Forgot password?</a>
-                        </div>
-                    </form>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  />
+                  <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                    Remember me
+                  </label>
                 </div>
 
-            </div>
-
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                />
-                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                  Remember me
-                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                    Forgot your password?
+                  </a>
+                </div>
               </div>
 
-              <div className="text-sm">
-                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                  Forgot your password?
-                </a>
-              </div>
-            </div>
+              <div>
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <span className="absolute left-0 inset-y-0 flex items-center pl-3">
 
-            <div>
-              <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                 
-                </span>
-                Sign in
-              </button>
-            </div>
-          </form>
+                  </span>
+                  Sign Up
+                </button>
+              </div>
+
+
+            </form>
+
+          </div>
         </div>
       </div>
     </>
-     );
+  );
 }
- 
+
 export default Signin;
